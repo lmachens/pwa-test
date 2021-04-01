e84f63366e92dbef8d254dffa67d9c96899d799e
lmachens <leon@machens.koeln>
2021-04-01 16:16:14 +0200

    Broadcast subscriptions

--- server/subscriptions.ts
+++ server/subscriptions.ts
@@ -1,4 +1,5 @@
 import { collection } from "./db";
+import webPush from "web-push";
 
 type Subscription = {
   endpoint: string;
@@ -12,4 +13,15 @@ const subscriptionCollection = () => collection<Subscription>("subscription");
 
 export const subscribe = (newSubscription: Subscription) => {
   return subscriptionCollection().insertOne(newSubscription);
+};
+
+export const broadcastMessage = async (message: string) => {
+  const subscriptions = await subscriptionCollection().find().toArray();
+  subscriptions.forEach((subscription) => {
+    const options = {
+      TTL: 60,
+    };
+
+    webPush.sendNotification(subscription, message, options);
+  });
 };
